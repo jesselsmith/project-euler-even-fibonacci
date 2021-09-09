d0cfeda302dd89c668982d720540e6ed71d1d84f
Jesse Smith <jesse.lee.smith@gmail.com>
2021-09-08 18:13:39 -0700

    commit tree e5d67222bfab82d352b6db5aa1f9e74b9f6eedb0

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,25 @@
 # Implement your procedural solution here!
+def even_fibonacci_sum(limit)
+    sequence = [1,2]
+    index = 1
+    total = 2
+    
+    while index < limit do
+        sum = sequence[index -1] + sequence[index]
+
+        if sum >= limit
+            return total
+        end
+        
+        sequence.push(sum)
+
+        if sum.even?
+            total += sum
+        end
+        
+        index += 1
+    end
+
+    total
+end
+
